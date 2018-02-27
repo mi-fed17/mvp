df0732b119c4cb02ef2f117f4e1ff5ce40fa72a1
Jesper Orb <jesper.orb@zocom.se>
2018-02-27 11:24:45 +0100

    Language added

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 getTodaysWeather();
 
 function getTodaysWeather(){
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=20a5e7348daf2de3c94dc3bbb83eede5&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=20a5e7348daf2de3c94dc3bbb83eede5&units=metric&lang=se')
     .then(function(response){
       return response.json();
     })
     .then(function(weatherData){
-      console.log(weatherData);
+      console.log(weatherData.weather[0].description);
+      console.log(weatherData.main.temp)
     })
     .catch(function(error){
       console.log(error);
