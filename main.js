7ab662cfb080abde45d06040c3e7a72632d97445
Jesper Orb <jesper.orb@zocom.se>
2018-02-27 11:16:09 +0100

    Metric system

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+getTodaysWeather();
+
 function getTodaysWeather(){
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=20a5e7348daf2de3c94dc3bbb83eede5&units=metric')
     .then(function(response){
       return response.json();
     })
