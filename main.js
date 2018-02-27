cceac3db650fe2f8c4c6aa82d6678159d3be9788
Jesper Orb <jesper.orb@zocom.se>
2018-02-27 11:06:58 +0100

    Added url

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function getID(id){
-  return document.getElementById(id);
+function getTodaysWeather(){
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(weatherData){
+      console.log(weatherData);
+    })
+    .catch(function(error){
+      console.log(error);
+    })
 }
-
-getID('myDiv');
